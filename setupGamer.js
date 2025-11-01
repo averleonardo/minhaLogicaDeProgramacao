311e1d27db3b99b5f758d6b2b91522c78f535697
Leonardo Aver <averleonardo@gmail.com>
2025-11-01 11:33:29 -0300

    Adiciona primeiro exercício em JavaScript (setup gamer)

---
+++ setupGamer.js
@@ -0,0 +1,25 @@
+// Itens do setup gamer
+let teclado = "Teclado Mecânico Redragon Kala, Switch Brown RGB";
+let mouse = "Mouse Gamer Razer Deathadder V2 16000 DPI";
+let monitor = "Monitor 144Hz";
+const tituloSetup = "Setup gamer da semana:\n";
+
+// Exibe o setup atual
+console.log(
+  tituloSetup +
+  teclado + " | " +
+  mouse + " | " +
+  monitor
+);
+
+// Atualizando item do setup
+teclado = "Teclado Mecânico 8BitDo N Edition, Bluetooth, 2.4G, USB-C";
+mouse = "Mouse Gamer Razer Deathadder V3 21000 DPI";
+monitor = "Monitor 360Hz";
+
+console.log(
+  "\nSetup atualizado:\n" +
+  teclado + " | " +
+  mouse + " | " +
+  monitor
+);
